9775d5d7c625fe84da4865fbad5768194ba57c1f
TonnyMusembi <musembitonnyj@gmail.com>
2024-09-12 12:30:07 +0300

    add

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HabitForm from "~/components/HabitForm.vue";
+import HabitList from "~/components/HabitList.vue";
+</script>
+<template>
+  <HabitList />
+  <HabitForm />
+  <div class="text-teal-500">test</div>
+</template>
