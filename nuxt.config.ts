67c66b2308a20be12f2036134be511af5a03a7b0
TonnyMusembi <musembitonnyj@gmail.com>
2024-09-18 15:04:37 +0300

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
+    "@nuxt/ui",
     "@nuxtjs/tailwindcss",
     "@pinia/nuxt",
     "pinia-plugin-persistedstate/nuxt",
     "@nuxt/eslint",
   ],
-
+colorMode: {
+    preference: 'light'
+  },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
 });
